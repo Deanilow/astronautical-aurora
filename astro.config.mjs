f2317b7896b483c93ce517c473ab5fc553289733
deanilow <d4niloRamos@hotmail.com>
2024-05-09 20:48:56 -0500

    td

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()],
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
---
+++ src/pages/blog/mi-primero-articulo.md
@@ -0,0 +1,21 @@
+---
+    title: 'Contenido de la pagina'
+    layout : '../../layouts/Layout.astro'
+---
+
+# Esto es un ejemplo de articulo 
+
+lorem ipsum dolor sit amet,
+
+## Subtitulo 
+
+lorem ipsum dolor sit amet,
+
+
+## Subtitulo 2
+
+```js
+
+function(){
+    console.log('Hola Mundo')
+}
